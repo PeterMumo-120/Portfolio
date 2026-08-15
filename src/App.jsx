e223fdcb48e3d3200e45b2ab5d98dc8ee75a984c
PeterMumo-120 <peter.wambua1@student.moringaschool.com>
2026-08-15 20:59:43 +0300

    Create src/App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is a React + Vite project</p>
+      </div>
+    </>
+  )
+}
+
+export default App
